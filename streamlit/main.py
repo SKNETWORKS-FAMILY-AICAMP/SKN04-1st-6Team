0e427a58ffe3c157a72bbdc7923c7909178d9ace
말랑곰 <leehoje000@gmail.com>
2024-08-23 15:05:55 +0900

    feat: 홈화면 보이도록 코드 작성
    기본 홈 화면과 홈버튼 누를때 홈페이지(차량등록현황)페이지 보이도록 코드 추가

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from views.car_faq_page import car_faq_page
-
+from views.homepage import hompage
 
 with st.sidebar:
     choice = option_menu("Menu", ["홈","Q&A"],
@@ -9,8 +9,9 @@ with st.sidebar:
                          
     )
 
+
 if choice == '홈':
-    pass
+    hompage()
 
 if choice == "Q&A":
     car_faq_page()
