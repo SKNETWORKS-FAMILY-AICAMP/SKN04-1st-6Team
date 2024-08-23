98dd39faf50763fa385fdf5518885ca0366fc159
말랑곰 <leehoje000@gmail.com>
2024-08-23 11:55:50 +0900

    feat: faq데이터 db로 넣는 기능 추가
    기아, 현대 faq데이터를 합쳐서 db로 넣는 작업 기능 추가
    
    TODO: 테스트목적으로 faq크롤링 개수를 줄놓았기 때문에 나중에 확인 필요.

--- crawling/runner.py
+++ crawling/runner.py
@@ -1,4 +1,4 @@
-from data_to_db import car_data_to_db
+from data_to_db import car_data_to_db, faq_data_to_db
 
 USERNAME = 'postgres'
 PASSWORD = '1234'
@@ -6,4 +6,5 @@ HOST = 'localhost'
 PORT = 5432
 DB_NAME = 'postgres'
 
-car_data_to_db(name=USERNAME, password=PASSWORD, host=HOST, port=PORT, db_name=DB_NAME)
+# car_data_to_db(name=USERNAME, password=PASSWORD, host=HOST, port=PORT, db_name=DB_NAME)
+faq_data_to_db(name=USERNAME, password=PASSWORD, host=HOST, port=PORT, db_name=DB_NAME)
