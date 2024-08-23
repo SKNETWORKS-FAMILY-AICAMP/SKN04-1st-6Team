12abbebbe4f499d9c7604b5d06c5cec757692065
말랑곰 <leehoje000@gmail.com>
2024-08-23 10:02:12 +0900

    feat: 크롤링 데이터db로 넣는 작업 작성 runner.py를 만들어 크롤링해온 데이터를 db로 넣는 스크립트작성 차량등록현황 db에 넣을때 적절한 타입으로 들어가도록 수정

--- crawling/data_to_db.py
+++ crawling/data_to_db.py
@@ -1,14 +1,21 @@
 import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.types import Integer, Text, String, DateTime, VARCHAR
-from crawling.registered_car import registrated_car_crawler
+from registered_car import registrated_car_crawler
 
 def car_data_to_db(**kwargs):
     data = registrated_car_crawler()
-    db_url = f'postgres+psycopg2://{kwargs.get('name')}:{kwargs.get('password')}@{kwargs.get('host')}:{kwargs.get('port')}/{kwargs.get('db_name')}'
+    db_url = f'postgresql+psycopg2://{kwargs.get('name')}:{kwargs.get('password')}@{kwargs.get('host')}:{kwargs.get('port')}/{kwargs.get('db_name')}'
     engine = create_engine(db_url)
 
     data.to_sql(
         'registererd_car',
         engine,
+        dtype={
+            'region': VARCHAR(5),
+            'count': Integer,
+            'type': VARCHAR(5),
+            'usage': VARCHAR(5),
+            'date': DateTime,
+        }
     )
